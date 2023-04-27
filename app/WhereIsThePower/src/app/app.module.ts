d35d9249d629b200899e24fc06e22e1b3c696a97
Andreas Visagie u21430901 <u21430901@tuks.co.za>
2023-04-27 02:05:46 +0200

    Update app.module.ts

--- app/WhereIsThePower/src/app/app.module.ts
+++ app/WhereIsThePower/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+// Webhook test
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
