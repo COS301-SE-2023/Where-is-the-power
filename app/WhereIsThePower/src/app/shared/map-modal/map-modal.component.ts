3268d2a2ea1ab67bfbafcc1845ab01187fcc9187
AmberLz <u18004874@tuks.co.za>
2023-05-21 12:29:50 +0200

    Added map-modal

---
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-modal',
+  templateUrl: './map-modal.component.html',
+  styleUrls: ['./map-modal.component.scss'],
+})
+export class MapModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
