a28f2ffb046ec9ddde06963bde3d3744e9d786f5
AmberLz <u18004874@tuks.co.za>
2023-06-24 16:38:44 +0200

    Create search component
    Also fixed config at angular.json

--- app/WhereIsThePower/src/app/app.module.ts
+++ app/WhereIsThePower/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './shared/search/search.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
