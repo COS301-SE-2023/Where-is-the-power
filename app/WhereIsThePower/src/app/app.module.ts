4cd9dfe7a9410884450a6bcad0cbed116791417b
AmberLz <u18004874@tuks.co.za>
2023-06-24 15:07:27 +0200

    Converted google maps to Mapbox

--- app/WhereIsThePower/src/app/app.module.ts
+++ app/WhereIsThePower/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LocationPickerComponent } from './shared/location-picker/location-picker.component';
 
 @NgModule({
   declarations: [AppComponent],
--- app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
@@ -2,81 +2,33 @@ import {
   Component,
   OnInit,
   AfterViewInit,
-  ViewChild,
-  ElementRef,
-  Renderer2
 } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { MapModalModule } from './map-modal.module';
 import { environment } from 'src/environments/environment';
-
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
 export class MapModalComponent implements OnInit, AfterViewInit {
-  @ViewChild('map', { static: false }) mapElementRef!: ElementRef;
-
   constructor(
-    private modalCtrl: ModalController,
-    private renderer: Renderer2
   ) { }
 
-  ngOnInit() { }
+  map: any;
+  ngOnInit() {
+  }
 
   ngAfterViewInit() {
-    this.getGoogleMaps()
-      .then(googleMaps => {
-        const mapEl = this.mapElementRef.nativeElement;
-        const map = new googleMaps.Map(mapEl, {
-          center: { lat: -25.774, lng: 28.261 },
-          zoom: 16
-        });
+    (mapboxgl as any).accessToken = environment.MapboxApiKey;
+    this.map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [28.261181, -25.771179], // starting position [lng, lat]
+      zoom: 12 // starting zoom
+    });
 
-        googleMaps.event.addListenerOnce(map, 'idle', () => {
-          this.renderer.addClass(mapEl, 'visible');
-        });
-
-        // tap for locations
-        map.addListener('click', (event: { latLng: { lat: () => any; lng: () => any; }; }) => {
-          const selectedCoords = {
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng()
-          };
-          this.modalCtrl.dismiss(selectedCoords);
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  onCancel() {
-    this.modalCtrl.dismiss();
-  }
-
-  private getGoogleMaps(): Promise<any> {
-    const win = window as any;
-    const googleModule = win.google;
-    if (googleModule && googleModule.maps) {
-      return Promise.resolve(googleModule.maps);
-    }
-    return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src =
-        'https://maps.googleapis.com/maps/api/js?key=' + environment.googleMapsApiKey;
-      script.async = true;
-      script.defer = true;
-      document.body.appendChild(script);
-      script.onload = () => {
-        const loadedGoogleModule = win.google;
-        if (loadedGoogleModule && loadedGoogleModule.maps) {
-          resolve(loadedGoogleModule.maps);
-        } else {
-          reject('Google maps SDK not available.');
-        }
-      };
+    this.map.on('load', () => {
+      this.map.resize(); // Trigger map resize after the initial rendering
     });
   }
 }
