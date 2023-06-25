73e968b0d55a959426a097543ddffedb9eb2e07c
AmberLz <u18004874@tuks.co.za>
2023-06-25 17:57:57 +0200

    Added Navigation feature
    Added navigation bar
    Removed old searchbar

--- app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
@@ -6,6 +6,8 @@ import {
 import { environment } from 'src/environments/environment';
 import * as mapboxgl from 'mapbox-gl';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+declare let MapboxDirections: any;
+
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
@@ -13,7 +15,6 @@ import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 })
 export class MapModalComponent implements OnInit, AfterViewInit {
   constructor() { }
-
   map: any;
   ngOnInit() {
   }
@@ -30,45 +31,24 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     this.map.on('load', () => {
       this.map.resize(); // Trigger map resize after the initial rendering
     });
-
-    const geocoder = new MapboxGeocoder({
-      // Initialize the geocoder
-      accessToken: environment.MapboxApiKey, // Set the access token
-      mapboxgl: mapboxgl, // Set the mapbox-gl instance
-      marker: false, // Do not use the default marker style
-      placeholder: 'Search for places', // Placeholder text for the search bar
-    });
+    /*
+        const geocoder = new MapboxGeocoder({
+          // Initialize the geocoder
+          accessToken: environment.MapboxApiKey, // Set the access token
+          mapboxgl: mapboxgl, // Set the mapbox-gl instance
+          marker: false, // Do not use the default marker style
+          placeholder: 'Search for places', // Placeholder text for the search bar
+    
+        });*/
 
     // Add the geocoder to the map
-    this.map.addControl(geocoder);
-
-    // After the map style has loaded on the page,
-    // add a source layer and default styling for a single point
-    this.map.on('load', () => {
-      this.map.addSource('single-point', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': []
-        }
-      });
-
-      this.map.addLayer({
-        'id': 'point',
-        'source': 'single-point',
-        'type': 'circle',
-        'paint': {
-          'circle-radius': 10,
-          'circle-color': '#448ee4'
-        }
-      });
-
-      // Listen for the `result` event from the Geocoder // `result` event is triggered when a user makes a selection
-      //  Add a marker at the result's coordinates
-      geocoder.on('result', (event: any) => {
-        this.map.getSource('single-point').setData(event.result.geometry);
-      });
-    });
+    // this.map.addControl(geocoder);
+    this.map.addControl(
+      new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+      }),
+      'top-left'
+    );
 
   }
 }
