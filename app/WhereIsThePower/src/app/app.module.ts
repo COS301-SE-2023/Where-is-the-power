51594b616aca43ccf0259372393dbedfa7dcebca
AmberLz <u18004874@tuks.co.za>
2023-06-25 14:14:50 +0200

    Added search feature

--- app/WhereIsThePower/src/app/app.module.ts
+++ app/WhereIsThePower/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './shared/search/search.component';
-
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
@@ -5,14 +5,14 @@ import {
 } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import * as mapboxgl from 'mapbox-gl';
+import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
 export class MapModalComponent implements OnInit, AfterViewInit {
-  constructor(
-  ) { }
+  constructor() { }
 
   map: any;
   ngOnInit() {
@@ -30,5 +30,46 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     this.map.on('load', () => {
       this.map.resize(); // Trigger map resize after the initial rendering
     });
+
+    const geocoder = new MapboxGeocoder({
+      // Initialize the geocoder
+      accessToken: environment.MapboxApiKey, // Set the access token
+      mapboxgl: mapboxgl, // Set the mapbox-gl instance
+      marker: false, // Do not use the default marker style
+      placeholder: 'Search for places', // Placeholder text for the search bar
+    });
+
+    // Add the geocoder to the map
+    this.map.addControl(geocoder);
+
+    // After the map style has loaded on the page,
+    // add a source layer and default styling for a single point
+    this.map.on('load', () => {
+      this.map.addSource('single-point', {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': []
+        }
+      });
+
+      this.map.addLayer({
+        'id': 'point',
+        'source': 'single-point',
+        'type': 'circle',
+        'paint': {
+          'circle-radius': 10,
+          'circle-color': '#448ee4'
+        }
+      });
+
+      // Listen for the `result` event from the Geocoder // `result` event is triggered when a user makes a selection
+      //  Add a marker at the result's coordinates
+      geocoder.on('result', (event: any) => {
+        this.map.getSource('single-point').setData(event.result.geometry);
+      });
+    });
+
   }
 }
+
