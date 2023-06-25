077d8ea6c117dd095d2dc32791f1dc38a07488fe
AmberLz <u18004874@tuks.co.za>
2023-06-25 18:05:33 +0200

    Added exclusion feature with Mock data
    Limited to 50 points for 'point(lng lat)'

--- app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.ts
@@ -38,7 +38,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
           mapboxgl: mapboxgl, // Set the mapbox-gl instance
           marker: false, // Do not use the default marker style
           placeholder: 'Search for places', // Placeholder text for the search bar
-    
+
         });*/
 
     // Add the geocoder to the map
@@ -46,9 +46,13 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     this.map.addControl(
       new MapboxDirections({
         accessToken: mapboxgl.accessToken,
+        unit: 'metric',
+        exclude: 'point(28.278153 -25.781812),point(28.277781 -25.78166),point(28.276252 -25.781039),point(28.274805 -25.780169),point(28.271878 -25.778368),point(28.271868 -25.778362),point(28.271357 -25.780567),point(28.272005 -25.780674),point(28.272028 -25.780909),point(28.272131 -25.781988)'
       }),
       'top-left'
     );
+
+
 
   }
 }
