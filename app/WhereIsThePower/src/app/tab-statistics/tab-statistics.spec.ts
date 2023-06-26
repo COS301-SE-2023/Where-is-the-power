8dd6901f937104ede486eecd445d1a174b01137c
Andreas Visagie u21430901 <angryaxe.visagie@gmail.com>
2023-06-26 20:23:11 +0200

    stats test

--- app/WhereIsThePower/src/app/tab-statistics/tab-statistics.spec.ts
+++ app/WhereIsThePower/src/app/tab-statistics/tab-statistics.spec.ts
@@ -1,10 +1,40 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By }  from '@angular/platform-browser';
+
 import { TabStatisticsPage } from './tab-statistics.page';
 
+describe('TabStaticsPage', () => {
+  let tabStaticsPage: TabStatisticsPage;
+  let fixture: ComponentFixture<TabStatisticsPage>;
+  let de: DebugElement;
 
-describe('Tab Statistics Page', () => {
-  it('should create an instance', () => {
-    let tabStatisticsPage = new TabStatisticsPage();
-    expect(tabStatisticsPage).toBeTruthy();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TabStatisticsPage ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+      fixture = TestBed.createComponent(TabStatisticsPage);
+      tabStaticsPage = fixture.componentInstance;
+      de = fixture.debugElement;
+
+      fixture.detectChanges();
   });
+
+  it('should create', () => {
+    expect(Component).toBeTruthy();
+  });
+
+  it('should create Doughnut Graph', () => {
+    expect(de.query(By.css('#doughnutChart'))).toBeTruthy();
+  });
+
+  it('should create Bar Chart', () => {
+    expect(de.query(By.css('#barChart'))).toBeTruthy();
+  });
+
+  
+
 });
