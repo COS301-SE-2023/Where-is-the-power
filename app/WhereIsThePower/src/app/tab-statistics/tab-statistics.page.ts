5a1a046e4a1f49243ed534975092c53fda43027b
AmberLz <u18004874@tuks.co.za>
2023-06-26 15:49:24 +0200

    Update tab-statistics.page.ts
    Added doughnut chart with mock data

--- app/WhereIsThePower/src/app/tab-statistics/tab-statistics.page.ts
+++ app/WhereIsThePower/src/app/tab-statistics/tab-statistics.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-tab-statistics',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab-statistics.page.scss'],
 })
 export class TabStatisticsPage implements OnInit {
+  @ViewChild('barChartRef') barChartRef!: ElementRef;
+  //@ViewChild('doughnutChart') doughnutChartRef!: ElementRef;
 
+  chart: any;
   constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.chart = new Chart("doughnutChart", {
+      type: 'doughnut',
+      data: {
+        labels: ['Uptime', 'Downtime'],
+        datasets: [{
+          label: 'Loadshedding',
+          data: [20, 4],
+          borderWidth: 0,
+          backgroundColor: [
+            '#007A4D',
+            '#DE3831',
+          ],
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        }
+      }
+    });
+
+
   }
-
 }
