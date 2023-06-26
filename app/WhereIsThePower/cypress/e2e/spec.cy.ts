d7dbd76b5e6a5f270634ed65c9489cd5d86c4132
TumiPare <u21452832@tuks.co.za>
2023-06-26 09:00:40 +0200

    E2E Navigation Test

--- app/WhereIsThePower/cypress/e2e/spec.cy.ts
+++ app/WhereIsThePower/cypress/e2e/spec.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" /> 
+
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/tabs/tab-schedule')
@@ -6,5 +8,27 @@ describe('My First Test', () => {
     cy.contains('Navigate')
     cy.contains('Statistics')
     cy.contains('Area')
+  })
+})
+
+
+describe('Navigation', () => {
+  it('should navigate to different tabs', () => {
+    cy.visit('/')
+    // make sure we are on navigate page initially
+    cy.location('pathname').should('eq', '/tabs/tab-navigate')
+    
+    cy.get('[data-cy="tab-saved"]').click()
+    cy.location('pathname').should('eq', '/tabs/tab-saved')
+
+    cy.get('[data-cy="tab-schedule"]').click()
+    cy.location('pathname').should('eq', '/tabs/tab-schedule')
+
+    cy.get('[data-cy="tab-statistics"]').click()
+    cy.location('pathname').should('eq', '/tabs/tab-statistics')
+
+    cy.get('[data-cy="tab-navigate"]').click()
+    cy.location('pathname').should('eq', '/tabs/tab-navigate')
+
   })
 })
