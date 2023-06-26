a793b96ec9db72d0ac80656dbf19c0b4e46430d5
AmberLz <u18004874@tuks.co.za>
2023-06-26 15:49:44 +0200

    Update tab-statistics.page.ts
    Added barchart with mock data

--- app/WhereIsThePower/src/app/tab-statistics/tab-statistics.page.ts
+++ app/WhereIsThePower/src/app/tab-statistics/tab-statistics.page.ts
@@ -40,6 +40,56 @@ export class TabStatisticsPage implements OnInit {
       }
     });
 
+    const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Uptime',
+          data: [20, 16, 20, 20, 12, 20, 24],
+          borderColor: '#007A4D',
+          backgroundColor: '#007A4D',
+        },
+        {
+          label: 'Downtime',
+          data: [-4, -8, -4, -4, -12, -4, 0],
+          borderColor: '#DE3831',
+          backgroundColor: '#DE3831',
+        }
+      ]
+    };
 
+    new Chart(this.barChartRef.nativeElement, {
+      type: 'bar',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            grid: {
+              display: true
+            }
+          }
+        },
+        layout: {
+          padding: {
+            left: 10,
+            right: 10,
+            top: 0,
+            bottom: 0
+          }
+        },
+      },
+    });
   }
 }
