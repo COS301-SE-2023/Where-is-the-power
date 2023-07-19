352c8923edc1893906c7c828bc8ce286f482b211
AmberLz <u18004874@tuks.co.za>
2023-07-19 19:50:12 +0200

    Update map-suburbs.service.ts
    Setup for http request

--- app/WhereIsThePower/src/app/shared/map-modal/map-suburbs.service.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-suburbs.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapSuburbsService {
+  apiUrl = 'https://witpa.codelog.co.za:8000/api/fetchMapData'
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      ContentType: 'application/json'
+    })
+  }
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getSuburbData() {
+
+  }
 }
