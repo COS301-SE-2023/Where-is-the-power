061877d14337b7c3ddabd1b538cc58cb02245796
AmberLz <u18004874@tuks.co.za>
2023-07-22 18:11:16 +0200

    Update tab-profile.page.ts
    Added call to login method with mock data

--- app/WhereIsThePower/src/app/tab-profile/tab-profile.page.ts
+++ app/WhereIsThePower/src/app/tab-profile/tab-profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterUser } from '../shared/models/register-user';
 import { AuthService } from '../authentication/auth.service';
+import { User } from '../shared/models/user';
 @Component({
   selector: 'app-tab-profile',
   templateUrl: './tab-profile.page.html',
@@ -14,15 +15,28 @@ export class TabProfilePage implements OnInit {
     password: "Password!123"
   };
 
+  User: User = {
+    authType: "User",
+    email: "jill@gmail.com",
+    password: "Password!123"
+  };
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
-    this.RegisterUser();
+    this.loginUser();
   }
 
   RegisterUser() {
     console.log(this.newUser)
     this.authService.signupUser(this.newUser).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
+
+  loginUser() {
+    console.log(this.User)
+    this.authService.loginUser(this.User).subscribe((response: any) => {
       console.log(response);
     });
   }
