f97e35339e3daa30dbcb5908219bf3b085eb981a
AmberLz <u18004874@tuks.co.za>
2023-07-22 17:54:08 +0200

    Update tab-profile.page.ts
    Register user method called with mock data

--- app/WhereIsThePower/src/app/tab-profile/tab-profile.page.ts
+++ app/WhereIsThePower/src/app/tab-profile/tab-profile.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RegisterUser } from '../shared/models/register-user';
+import { AuthService } from '../authentication/auth.service';
 @Component({
   selector: 'app-tab-profile',
   templateUrl: './tab-profile.page.html',
   styleUrls: ['./tab-profile.page.scss'],
 })
 export class TabProfilePage implements OnInit {
+  newUser: RegisterUser = {
+    firstName: "Jill",
+    lastName: "Moore",
+    email: "jill@gmail.com",
+    password: "Password!123"
+  };
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.RegisterUser();
   }
 
+  RegisterUser() {
+    console.log(this.newUser)
+    this.authService.signupUser(this.newUser).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
 }
