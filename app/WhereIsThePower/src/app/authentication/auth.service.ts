8b13f7e07e0827163d25b12431072367eb9ef01c
AmberLz <u18004874@tuks.co.za>
2023-07-22 17:53:37 +0200

    Update auth.service.ts
    Added signup request

--- app/WhereIsThePower/src/app/authentication/auth.service.ts
+++ app/WhereIsThePower/src/app/authentication/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { RegisterUser } from '../shared/models/register-user';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  apiUrl = 'http://witpa.codelog.co.za/api/'
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  signupUser(registerUser: RegisterUser) {
+    return this.httpClient.post(`${this.apiUrl}user`, registerUser)
+  }
+
 }
