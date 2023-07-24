d7a8317d7b58cea31a986c8c54af80ccf362fe03
AmberLz <u18004874@tuks.co.za>
2023-07-24 14:04:57 +0200

    Update user.ts
    Add user class

--- app/WhereIsThePower/src/app/shared/models/user.ts
+++ app/WhereIsThePower/src/app/shared/models/user.ts
@@ -1,6 +1,15 @@
-export interface User {
+export interface UserInterface {
   authType: string;
   email: string;
   password: string;
   token?: string;
+}
+
+export class User implements UserInterface {
+  constructor(
+    public authType: string,
+    public email: string,
+    public password: string,
+    public token?: string,
+  ) { }
 }
