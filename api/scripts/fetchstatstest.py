bfda9c031ccae069f9077a917f496c970ff9a294
GD Radloff <danielrad082@gmail.com>
2023-07-28 16:26:50 +0200

    refactored stats response and added e2e python tester

---
+++ api/scripts/fetchstatstest.py
@@ -0,0 +1,19 @@
+import sys
+import json
+import requests
+
+testing_endpoint = "http://witpa.codelog.co.za/api/fetchSuburbStats"
+testing_endpoint = "http://127.0.0.1:8000/api/fetchSuburbStats"
+def sendRequest():
+    body = {
+      "suburbId" : 17959
+    }
+    request = json.dumps(body)
+    headers = {
+      "Content-Type":"application/json"
+    }
+    response = requests.post(url=testing_endpoint,data=request,headers=headers)
+    print(response.text)
+
+if (__name__ == "__main__"):
+    sendRequest()
