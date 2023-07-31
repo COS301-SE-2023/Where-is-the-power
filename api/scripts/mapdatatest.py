ec0d1f68ed260f51330225fc54ed94558ef1e966
GD Radloff <danielrad082@gmail.com>
2023-07-31 22:31:56 +0200

    Hotfix, comparison schedule bug

--- api/scripts/mapdatatest.py
+++ api/scripts/mapdatatest.py
@@ -7,7 +7,8 @@ testing_endpoint = "http://127.0.0.1:8000/api/fetchMapData"
 def sendRequest():
     body = {
         "bottomLeft": [-90,-180],
-        "topRight": [90,180]
+        "topRight": [90,180],
+        "time" : 1690834649
     }
     request = json.dumps(body)
     headers = {
