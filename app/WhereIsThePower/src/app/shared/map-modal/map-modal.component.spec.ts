0f81046d1551e1e80a38b145dc69a74464148479
TumiPare <u21452832@tuks.co.za>
2023-08-01 00:58:08 +0200

    Test: should call getSuburbData() and populate map with polygons

--- app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.spec.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.spec.ts
@@ -1,12 +1,15 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
 import { MapModalComponent } from "./map-modal.component";
 import { CUSTOM_ELEMENTS_SCHEMA,ChangeDetectorRef} from '@angular/core';
 import { MapSuburbsService } from './map-suburbs.service';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { IonContent, ModalController, AngularDelegate} from '@ionic/angular';
+import { of } from 'rxjs';
 
 
 import { UserLocationService } from '../../user-location.service';
+
+declare let mapboxgl: any;
 
 
 
@@ -16,6 +19,8 @@ import { UserLocationService } from '../../user-location.service';
 describe("MapModalComponent", () =>{
     let component: MapModalComponent;
     let fixture: ComponentFixture<MapModalComponent>;
+    let mapSuburbsService: MapSuburbsService;
+    let userLocationService: UserLocationService;
 
     beforeEach(() => {
         // Initialize the testing environment and create the component fixture
@@ -27,6 +32,9 @@ describe("MapModalComponent", () =>{
         }).compileComponents();
         fixture = TestBed.createComponent(MapModalComponent);
         component = fixture.componentInstance;
+        mapSuburbsService = TestBed.inject(MapSuburbsService);
+        userLocationService = TestBed.inject(UserLocationService);
+        fixture.detectChanges();
     });
 
     it('should create the MapModalComponent', () => {
@@ -36,5 +44,36 @@ describe("MapModalComponent", () =>{
     it("should render the map", () =>{
 
     });
+    xit('should call getSuburbData() and populate map with polygons', (done) => {
+        spyOn(mapSuburbsService, 'getSuburbData').and.callThrough();
+        spyOn(component, 'populatePolygons');
+        // spyOn(component, 'ngAfterViewInit').and.callThrough;
+    
+        // Trigger the ngAfterViewInit function
+        try {
+            component.ngAfterViewInit();
+        } catch(error) {
+            fail('ngAfterViewInit threw an error: ' + error);
+        }
+
+        
+
+        
+    
+        // Check if getSuburbData was called
+        expect(mapSuburbsService.getSuburbData).toHaveBeenCalled();
+    
+        fixture.whenStable().then(() => {
+            // Check if populatePolygons was called after the subscription completes
+            expect(component.populatePolygons).toHaveBeenCalled();
+            done();
+        })
+        
+        // expect(component.map).toBeTruthy();
+    
+        // Additional tests can be done based on the behavior of your component.
+        // For instance, you can check if the map is initialized with the correct properties.
+      });
+    
 }
 );
