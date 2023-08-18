0333fc34c164140836c9a97e7468a8fd6eb35ffa
AmberLz <u18004874@tuks.co.za>
2023-08-18 20:36:51 +0200

    Update report.page.ts

--- app/WhereIsThePower/src/app/report/report.page.ts
+++ app/WhereIsThePower/src/app/report/report.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReportService } from './report.service';
 import { Subscription } from 'rxjs';
+import { UserLocationService } from '../user-location.service';
 
 @Component({
   selector: 'app-report',
@@ -9,12 +10,18 @@ import { Subscription } from 'rxjs';
 })
 export class ReportPage implements OnInit {
   private createReportSubscription: Subscription = new Subscription();
+  latitude: any;
+  longitude: any;
 
   constructor(
-    private reportService: ReportService
+    private reportService: ReportService,
+    private userLocationService: UserLocationService  
   ) { }
 
   ngOnInit() {
+    this.latitude = this.userLocationService.getLatitude();
+    this.longitude = this.userLocationService.getLongitude();
+
     this.reportService.getReports().subscribe((data) => {
       console.log(data);
     });
