330b3739e2ddfa4ef2b505bbd65933caa91b0edc
AmberLz <u18004874@tuks.co.za>
2023-08-18 21:04:37 +0200

    Added prompt to login when reporting

--- app/WhereIsThePower/src/app/report/report.page.ts
+++ app/WhereIsThePower/src/app/report/report.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReportService } from './report.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AuthService } from '../authentication/auth.service';
 
 @Component({
   selector: 'app-report',
@@ -10,16 +11,22 @@ import { Router } from '@angular/router';
 })
 export class ReportPage implements OnInit {
   private createReportSubscription: Subscription = new Subscription();
+  isLoggedIn: boolean = false;
 
   constructor(
     private reportService: ReportService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
     this.reportService.getReports().subscribe((data) => {
       console.log(data);
     });
+  }
+
+  async ionViewWillEnter() {
+    this.isLoggedIn = await this.authService.isUserLoggedIn();
   }
 
   report(reportType: string) {
