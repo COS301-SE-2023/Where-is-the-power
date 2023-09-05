0f442428559e2ac08704800b76d40ac484a0c8fe
TumiPare <u21452832@tuks.co.za>
2023-09-05 21:08:09 +0200

    got tileset, and exported info as json

--- api/scripts/findRobots.py
+++ api/scripts/findRobots.py
@@ -1,49 +1,40 @@
-#this script finds all the Traffic lights in the city of Tswhane
+import math
 import requests
+import mapbox_vector_tile
 import json
-import numpy as np
-from scipy.spatial import Delaunay 
 
-def get_all_polygons():
-    api_url = 'https://witpa.codelog.co.za/api/fetchMapData'
+def deg2num(lat_deg, lon_deg, zoom):
+  lat_rad = math.radians(lat_deg)
+  n = 1 << zoom
+  xtile = int((lon_deg + 180.0) / 360.0 * n)
+  ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+  return xtile, ytile
 
-    # Define the request payload (body)
-    body = {
-        "bottomLeft": [-90, -180],
-        "topRight": [90, 180]
+def num2deg(xtile, ytile, zoom):
+  n = 1 << zoom
+  lon_deg = xtile / n * 360.0 - 180.0
+  lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+  lat_deg = math.degrees(lat_rad)
+  return lat_deg, lon_deg
+
+def get_tileset():
+    x,y = deg2num(-25.821315,28.260659,10)
+    endpoint = f"https://api.mapbox.com/v4/mapbox.mapbox-traffic-v1/10/{x}/{y}.mvt"
+    params = {
+        "access_token": access_token
     }
+    response = requests.get(endpoint, params=params)
+    print(response.status_code)
+    return response.content
 
-    # Send a POST request to the API
-    witpaData = requests.post(api_url, json=body).json()
+output = mapbox_vector_tile.decode(get_tileset())
+json_data = json.dumps(output, indent=4)
+# print(output["admin"])
 
-    polygons = []
-    for feature in witpaData['result']['mapPolygons'][0]['features']:
-            polygon = [point for point in feature['geometry']['coordinates'][0] if len(point) == 2]
-            sub_polygon = [point for point in feature['geometry']['coordinates'][0] if len(point) > 2]
-            polygons.append(polygon)
-    return polygons
+with open("output.json", "w") as f:
+    f.write(json_data)
 
-polygons = get_all_polygons()
-coordinates = []
+print(f'Data has been saved.')
 
-for polygon in polygons:
-     for point in polygon:
-          coordinates.append(point)
 
-points = np.array(coordinates)
-tri = Delaunay(points)
-hull = tri.convex_hull
-convex_points = points[hull]
-
-print(convex_points)
-
-# def eliminate_points_inside_hull(points, convex_hull):
-#     a, b, c = np.cross(points[convex_hull[0]], points[convex_hull[1]])
-#     mask = np.dot(points, np.array([a, b])) + c <= 0
-#     return points[mask]
-
-# eliminate_points_inside_hull(points, convex_points)
-# print(eliminate_points_inside_hull(points, convex_points))
-# # Save the coordinates to a file (e.g., "big_polygon_coords.json")
-# with open("tshwane", "w") as output_file:
-#     json.dump(convex_points.tolist(), output_file, indent=2)
+    
