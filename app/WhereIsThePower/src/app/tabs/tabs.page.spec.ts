7bec70d486dfbccb221f975f5a892f2563f2c500
TumiPare <u21452832@tuks.co.za>
2023-09-27 14:51:53 +0200

    tabs page test

---
+++ app/WhereIsThePower/src/app/tabs/tabs.page.spec.ts
@@ -0,0 +1,75 @@
+// @ts-nocheck
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
+
+import { Component } from '@angular/core';
+import { TabsPage } from './tabs.page';
+import { MapSuburbsService } from '../shared/map-modal/map-suburbs.service';
+
+@Injectable()
+class MockMapSuburbsService {}
+
+@Directive({ selector: '[myCustom]' })
+class MyCustomDirective {
+  @Input() myCustom;
+}
+
+@Pipe({name: 'translate'})
+class TranslatePipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+@Pipe({name: 'phoneNumber'})
+class PhoneNumberPipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+@Pipe({name: 'safeHtml'})
+class SafeHtmlPipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+describe('TabsPage', () => {
+  let fixture;
+  let component;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      declarations: [
+        TabsPage,
+        TranslatePipe, PhoneNumberPipe, SafeHtmlPipe,
+        MyCustomDirective
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: MapSuburbsService, useClass: MockMapSuburbsService }
+      ]
+    }).overrideComponent(TabsPage, {
+
+    }).compileComponents();
+    fixture = TestBed.createComponent(TabsPage);
+    component = fixture.debugElement.componentInstance;
+  });
+
+  afterEach(() => {
+    component.ngOnDestroy = function() {};
+    fixture.destroy();
+  });
+
+  it('should run #constructor()', async () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should run #ngOnInit()', async () => {
+    component.MapSuburbsService = component.MapSuburbsService || {};
+    component.MapSuburbsService.gettingDirections = observableOf({});
+    component.ngOnInit();
+
+  });
+
+});
