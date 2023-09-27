1e477ccc52784ce4fa7f4bce2225b3a8bce6b306
TumiPare <u21452832@tuks.co.za>
2023-09-27 14:48:22 +0200

    tab-schedule test

---
+++ app/WhereIsThePower/src/app/tab-schedule/tab-schedule.page.spec.ts
@@ -0,0 +1,163 @@
+// @ts-nocheck
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
+
+import { Component } from '@angular/core';
+import { TabSchedulePage } from './tab-schedule.page';
+import { UserLocationService } from '../user-location.service';
+import { ScheduleService } from './schedule.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+class MockUserLocationService {}
+
+@Injectable()
+class MockScheduleService {}
+
+@Injectable()
+class MockHttpClient {
+  post() {};
+}
+
+@Directive({ selector: '[myCustom]' })
+class MyCustomDirective {
+  @Input() myCustom;
+}
+
+@Pipe({name: 'translate'})
+class TranslatePipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+@Pipe({name: 'phoneNumber'})
+class PhoneNumberPipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+@Pipe({name: 'safeHtml'})
+class SafeHtmlPipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+describe('TabSchedulePage', () => {
+  let fixture;
+  let component;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      declarations: [
+        TabSchedulePage,
+        TranslatePipe, PhoneNumberPipe, SafeHtmlPipe,
+        MyCustomDirective
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: UserLocationService, useClass: MockUserLocationService },
+        { provide: ScheduleService, useClass: MockScheduleService },
+        { provide: HttpClient, useClass: MockHttpClient }
+      ]
+    }).overrideComponent(TabSchedulePage, {
+
+    }).compileComponents();
+    fixture = TestBed.createComponent(TabSchedulePage);
+    component = fixture.debugElement.componentInstance;
+  });
+
+  afterEach(() => {
+    component.ngOnDestroy = function() {};
+    fixture.destroy();
+  });
+
+  it('should run #constructor()', async () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should run #ngOnInit()', async () => {
+    component.http = component.http || {};
+    component.http.get = jest.fn().mockReturnValue(observableOf({}));
+    component.scheduleService = component.scheduleService || {};
+    component.scheduleService.getLoadSheddingStage = jest.fn().mockReturnValue(observableOf({
+      result: {}
+    }));
+    component.setChipColor = jest.fn();
+    await component.ngOnInit();
+    // expect(component.http.get).toHaveBeenCalled();
+    // expect(component.scheduleService.getLoadSheddingStage).toHaveBeenCalled();
+    // expect(component.setChipColor).toHaveBeenCalled();
+  });
+
+  it('should run #ionViewWillEnter()', async () => {
+    component.userLocationService = component.userLocationService || {};
+    component.userLocationService.getUserLocation = jest.fn();
+    component.userLocationService.isLocationAvailable = observableOf({});
+    component.userLocationService.getArea = jest.fn();
+    component.selectSuburb = jest.fn();
+    await component.ionViewWillEnter();
+    // expect(component.userLocationService.getUserLocation).toHaveBeenCalled();
+    // expect(component.userLocationService.getArea).toHaveBeenCalled();
+    // expect(component.selectSuburb).toHaveBeenCalled();
+  });
+
+  it('should run #onSearch()', async () => {
+    component.searchTerm = component.searchTerm || {};
+    component.searchTerm.toLowerCase = jest.fn();
+    component.searchItems = component.searchItems || {};
+    component.searchItems = ['searchItems'];
+    component.onSearch({});
+    // expect(component.searchTerm.toLowerCase).toHaveBeenCalled();
+  });
+
+  it('should run #onBlur()', async () => {
+
+    component.onBlur();
+
+  });
+
+  it('should run #undefined()', async () => {
+    // Error: ERROR this JS code is invalid, "data.result.timesOff.forEach((timeOff)"
+    //     at Function.getFuncReturn (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\util.js:325:13)
+    //     at C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\util.js:413:30
+    //     at Array.forEach (<anonymous>)
+    //     at Function.getFuncParamObj (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\util.js:396:26)
+    //     at Function.getFuncArguments (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\util.js:347:30)
+    //     at Function.getFuncReturn (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\util.js:332:34)
+    //     at FuncTestGen.setMockData (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\func-test-gen.js:159:31)
+    //     at FuncTestGen.setMockData (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\func-test-gen.js:154:14)
+    //     at FuncTestGen.setMockData (C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\func-test-gen.js:90:12)
+    //     at C:\Users\tumis\Documents\Where-is-the-power\app\WhereIsThePower\node_modules\ngentest\lib\index.js:188:17
+  });
+
+  it('should run #convertToDateTime()', async () => {
+
+    component.convertToDateTime({});
+
+  });
+
+  it('should run #formatTime()', async () => {
+
+    component.formatTime({});
+
+  });
+
+  it('should run #setChipColor()', async () => {
+
+    component.setChipColor({});
+
+  });
+
+  it('should run #ngOnDestroy()', async () => {
+    component.suburbDataSubscription = component.suburbDataSubscription || {};
+    component.suburbDataSubscription.unsubscribe = jest.fn();
+    component.listSuburbsSubscription = component.listSuburbsSubscription || {};
+    component.listSuburbsSubscription.unsubscribe = jest.fn();
+    component.ngOnDestroy();
+    // expect(component.suburbDataSubscription.unsubscribe).toHaveBeenCalled();
+    // expect(component.listSuburbsSubscription.unsubscribe).toHaveBeenCalled();
+  });
+
+});Error: ERROR this JS code is invalid, "data.result.timesOff.forEach((timeOff)"
