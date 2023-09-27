a0ddaed8eb18b04858709fcbb153476a32fbc922
TumiPare <u21452832@tuks.co.za>
2023-09-27 14:57:17 +0200

    signup unit test

---
+++ app/WhereIsThePower/src/app/shared/components/signup/signup.component.spec.ts
@@ -0,0 +1,154 @@
+// @ts-nocheck
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
+
+import { Component } from '@angular/core';
+import { SignupComponent } from './signup.component';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { ToastController, ModalController, LoadingController } from '@ionic/angular';
+import { AuthService } from '../../../authentication/auth.service';
+
+@Injectable()
+class MockRouter {
+  navigate() {};
+}
+
+@Injectable()
+class MockAuthService {}
+
+@Directive({ selector: '[myCustom]' })
+class MyCustomDirective {
+  @Input() myCustom;
+}
+
+@Pipe({name: 'translate'})
+class TranslatePipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+@Pipe({name: 'phoneNumber'})
+class PhoneNumberPipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+@Pipe({name: 'safeHtml'})
+class SafeHtmlPipe implements PipeTransform {
+  transform(value) { return value; }
+}
+
+describe('SignupComponent', () => {
+  let fixture;
+  let component;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      declarations: [
+        SignupComponent,
+        TranslatePipe, PhoneNumberPipe, SafeHtmlPipe,
+        MyCustomDirective
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: Router, useClass: MockRouter },
+        FormBuilder,
+        ToastController,
+        { provide: AuthService, useClass: MockAuthService },
+        ModalController,
+        LoadingController
+      ]
+    }).overrideComponent(SignupComponent, {
+
+    }).compileComponents();
+    fixture = TestBed.createComponent(SignupComponent);
+    component = fixture.debugElement.componentInstance;
+  });
+
+  afterEach(() => {
+    component.ngOnDestroy = function() {};
+    fixture.destroy();
+  });
+
+  it('should run #constructor()', async () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should run #ngOnInit()', async () => {
+
+    component.ngOnInit();
+
+  });
+
+  it('should run #dismissModal()', async () => {
+    component.modalController = component.modalController || {};
+    component.modalController.dismiss = jest.fn();
+    component.dismissModal();
+    // expect(component.modalController.dismiss).toHaveBeenCalled();
+  });
+
+  it('should run #signup()', async () => {
+    component.signupForm = component.signupForm || {};
+    component.signupForm.valid = 'valid';
+    component.signupForm.value = {
+      firstName: {},
+      lastName: {},
+      email: {},
+      password: {}
+    };
+    component.newUser = component.newUser || {};
+    component.newUser.firstName = 'firstName';
+    component.newUser.lastName = 'lastName';
+    component.newUser.email = 'email';
+    component.newUser.password = 'password';
+    component.authService = component.authService || {};
+    component.authService.signupUser = jest.fn().mockReturnValue(observableOf({
+      token: {}
+    }));
+    component.authService.loginUser = jest.fn().mockReturnValue(observableOf({
+      token: {}
+    }));
+    component.authService.saveUserData = jest.fn();
+    component.authService.user = {
+      next: function() {}
+    };
+    component.presentLoading = jest.fn();
+    component.dismissModal = jest.fn();
+    component.sucessToast = jest.fn();
+    component.failToast = jest.fn();
+    component.signup();
+    // expect(component.authService.signupUser).toHaveBeenCalled();
+    // expect(component.authService.loginUser).toHaveBeenCalled();
+    // expect(component.authService.saveUserData).toHaveBeenCalled();
+    // expect(component.presentLoading).toHaveBeenCalled();
+    // expect(component.dismissModal).toHaveBeenCalled();
+    // expect(component.sucessToast).toHaveBeenCalled();
+    // expect(component.failToast).toHaveBeenCalled();
+  });
+
+  it('should run #failToast()', async () => {
+    component.toastController = component.toastController || {};
+    component.toastController.create = jest.fn();
+    await component.failToast({});
+    // expect(component.toastController.create).toHaveBeenCalled();
+  });
+
+  it('should run #sucessToast()', async () => {
+    component.toastController = component.toastController || {};
+    component.toastController.create = jest.fn();
+    await component.sucessToast({});
+    // expect(component.toastController.create).toHaveBeenCalled();
+  });
+
+  it('should run #presentLoading()', async () => {
+    component.loadingController = component.loadingController || {};
+    component.loadingController.create = jest.fn();
+    await component.presentLoading();
+    // expect(component.loadingController.create).toHaveBeenCalled();
+  });
+
+});
