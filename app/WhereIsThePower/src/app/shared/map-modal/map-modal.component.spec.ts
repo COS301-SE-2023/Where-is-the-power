48a922418a7d77d313650fc893bcc692bf51f3e1
TumiPare <u21452832@tuks.co.za>
2023-09-27 15:42:05 +0200

    fixed map-modal unit test

--- app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.spec.ts
+++ app/WhereIsThePower/src/app/shared/map-modal/map-modal.component.spec.ts
@@ -111,8 +111,8 @@ describe('MapModalComponent', () => {
     };
     component.savedPlacesService.navigateToSavedPlace = observableOf({});
     component.map = component.map || {};
-    component.map.flyTo = jest.fn();
-    component.openNavigateModal = jest.fn();
+    spyOn(component.map, 'flyTo');
+    spyOn(component, 'openNavigateModal');
     component.ngOnInit();
     // expect(component.map.flyTo).toHaveBeenCalled();
     // expect(component.openNavigateModal).toHaveBeenCalled();
@@ -133,24 +133,24 @@ describe('MapModalComponent', () => {
   });
 
   it('should run #addMarker()', async () => {
-    component.closePopup = jest.fn();
+    spyOn(component, 'closePopup');
     component.addMarker({}, {}, 'reportType');
     // expect(component.closePopup).toHaveBeenCalled();
   });
 
   it('should run #populatePolygons()', async () => {
     component.map = component.map || {};
-    component.map.on = jest.fn();
-    component.map.addSource = jest.fn();
-    component.map.addLayer = jest.fn();
+    spyOn(component.map, 'on');
+    spyOn(component.map, 'addSource');
+    spyOn(component.map, 'addLayer');
     component.mapSuburbsService = component.mapSuburbsService || {};
-    component.mapSuburbsService.fetchTimeForPolygon = jest.fn().mockReturnValue(observableOf({
+    spyOn(component.mapSuburbsService, 'fetchTimeForPolygon').and.returnValue(observableOf({
       success: {},
       result: {
         timesOff: {}
       }
     }));
-    component.mapSuburbsService.getSuburbData = jest.fn();
+    spyOn(component.mapSuburbsService, 'getSuburbData');
     component.populatePolygons();
     // expect(component.map.on).toHaveBeenCalled();
     // expect(component.map.addSource).toHaveBeenCalled();
@@ -190,25 +190,25 @@ describe('MapModalComponent', () => {
 
   it('should run #getRoute()', async () => {
     component.instructions = component.instructions || {};
-    component.instructions.push = jest.fn();
-    component.updateBreakpoint = jest.fn();
-    component.emitGetDirections = jest.fn();
-    component.closePopup = jest.fn();
-    component.cancelNavigateModal = jest.fn();
-    component.openModal = jest.fn();
-    component.presentLoading = jest.fn();
+    spyOn(component.instructions, 'push');
+    spyOn(component, 'updateBreakpoint');
+    spyOn(component, 'emitGetDirections');
+    spyOn(component, 'closePopup');
+    spyOn(component, 'cancelNavigateModal');
+    spyOn(component, 'openModal');
+    spyOn(component, 'presentLoading');
     component.mapSuburbsService = component.mapSuburbsService || {};
-    component.mapSuburbsService.fetchOptimalRoute = jest.fn().mockReturnValue(observableOf({}));
+    spyOn(component.mapSuburbsService, 'fetchOptimalRoute').and.returnValue(observableOf({}));
     component.searchBar = component.searchBar || {};
     component.searchBar.value = 'value';
     component.map = component.map || {};
-    component.map.getLayer = jest.fn();
-    component.map.getSource = jest.fn().mockReturnValue({
+    spyOn(component.map, 'getLayer');
+    spyOn(component.map, 'getSource').and.returnValue({
       setData: function() {}
     });
-    component.map.addLayer = jest.fn();
-    component.map.fitBounds = jest.fn();
-    component.calculateETA = jest.fn();
+    spyOn(component.map, 'addLayer');
+    spyOn(component.map, 'fitBounds');
+    spyOn(component, 'calculateETA');
     await component.getRoute({
       hasOwnProperty: function() {},
       longitude: {},
@@ -237,7 +237,7 @@ describe('MapModalComponent', () => {
 
   it('should run #presentLoading()', async () => {
     component.loadingController = component.loadingController || {};
-    component.loadingController.create = jest.fn();
+    spyOn(component.loadingController, 'create');
     await component.presentLoading();
     // expect(component.loadingController.create).toHaveBeenCalled();
   });
@@ -250,14 +250,14 @@ describe('MapModalComponent', () => {
 
   it('should run #onSearchBarClear()', async () => {
     component.myModal = component.myModal || {};
-    component.myModal.dismiss = jest.fn();
-    component.emitCancelDirections = jest.fn();
-    component.updateBreakpoint = jest.fn();
+    spyOn(component.myModal, 'dismiss');
+    spyOn(component, 'emitCancelDirections');
+    spyOn(component, 'updateBreakpoint');
     component.map = component.map || {};
-    component.map.getSource = jest.fn();
-    component.map.removeLayer = jest.fn();
-    component.map.removeSource = jest.fn();
-    component.map.getLayer = jest.fn();
+    spyOn(component.map, 'getSource');
+    spyOn(component.map, 'removeLayer');
+    spyOn(component.map, 'removeSource');
+    spyOn(component.map, 'getLayer');
     component.onSearchBarClear();
     // expect(component.myModal.dismiss).toHaveBeenCalled();
     // expect(component.emitCancelDirections).toHaveBeenCalled();
@@ -270,8 +270,8 @@ describe('MapModalComponent', () => {
 
   it('should run #centerOnStartPoint()', async () => {
     component.map = component.map || {};
-    component.map.flyTo = jest.fn();
-    component.closePopup = jest.fn();
+    spyOn(component.map, 'flyTo');
+    spyOn(component, 'closePopup');
     component.centerOnStartPoint();
     // expect(component.map.flyTo).toHaveBeenCalled();
     // expect(component.closePopup).toHaveBeenCalled();
@@ -279,15 +279,15 @@ describe('MapModalComponent', () => {
 
   it('should run #openModal()', async () => {
     component.myModal = component.myModal || {};
-    component.myModal.present = jest.fn();
+    spyOn(component.myModal, 'present');
     component.openModal({});
     // expect(component.myModal.present).toHaveBeenCalled();
   });
 
   it('should run #openNavigateModal()', async () => {
     component.navigateModal = component.navigateModal || {};
-    component.navigateModal.present = jest.fn();
-    component.updateBreakpoint = jest.fn();
+    spyOn(component.navigateModal, 'present');
+    spyOn(component, 'updateBreakpoint');
     component.openNavigateModal();
     // expect(component.navigateModal.present).toHaveBeenCalled();
     // expect(component.updateBreakpoint).toHaveBeenCalled();
@@ -295,10 +295,10 @@ describe('MapModalComponent', () => {
 
   it('should run #calculateETA()', async () => {
     component.tripETA = component.tripETA || {};
-    component.tripETA.setHours = jest.fn();
-    component.tripETA.getHours = jest.fn();
-    component.tripETA.setMinutes = jest.fn();
-    component.tripETA.getMinutes = jest.fn();
+    spyOn(component.tripETA, 'setHours');
+    spyOn(component.tripETA, 'getHours');
+    spyOn(component.tripETA, 'setMinutes');
+    spyOn(component.tripETA, 'getMinutes');
     component.calculateETA();
     // expect(component.tripETA.setHours).toHaveBeenCalled();
     // expect(component.tripETA.getHours).toHaveBeenCalled();
@@ -313,10 +313,10 @@ describe('MapModalComponent', () => {
   });
 
   it('should run #beginTrip()', async () => {
-    component.centerOnStartPoint = jest.fn();
-    component.updateBreakpoint = jest.fn();
+    spyOn(component, 'centerOnStartPoint');
+    spyOn(component, 'updateBreakpoint');
     component.userMarker = component.userMarker || {};
-    component.userMarker.remove = jest.fn();
+    spyOn(component.userMarker, 'remove');
     component.beginTrip();
     // expect(component.centerOnStartPoint).toHaveBeenCalled();
     // expect(component.updateBreakpoint).toHaveBeenCalled();
@@ -324,16 +324,16 @@ describe('MapModalComponent', () => {
   });
 
   it('should run #pin()', async () => {
-    component.centerOnStartPoint = jest.fn();
+    spyOn(component, 'centerOnStartPoint');
     component.userMarker = component.userMarker || {};
-    component.userMarker.remove = jest.fn();
+    spyOn(component.userMarker, 'remove');
     component.pin();
     // expect(component.centerOnStartPoint).toHaveBeenCalled();
     // expect(component.userMarker.remove).toHaveBeenCalled();
   });
 
   it('should run #onModalDismiss()', async () => {
-    component.onSearchBarClear = jest.fn();
+    spyOn(component, 'onSearchBarClear');
     component.onModalDismiss();
     // expect(component.onSearchBarClear).toHaveBeenCalled();
   });
@@ -344,7 +344,7 @@ describe('MapModalComponent', () => {
       next: function() {}
     };
     component.map = component.map || {};
-    component.map.resize = jest.fn();
+    spyOn(component.map, 'resize');
     component.emitCancelDirections();
     // expect(component.map.resize).toHaveBeenCalled();
   });
@@ -354,30 +354,30 @@ describe('MapModalComponent', () => {
     component.mapSuburbsService.gettingDirections = {
       next: function() {}
     };
-    component.delay = jest.fn();
+    spyOn(component, 'delay');
     component.map = component.map || {};
-    component.map.resize = jest.fn();
+    spyOn(component.map, 'resize');
     await component.emitGetDirections();
     // expect(component.delay).toHaveBeenCalled();
     // expect(component.map.resize).toHaveBeenCalled();
   });
 
   it('should run #onResize()', async () => {
-    component.updateBreakpoint = jest.fn();
+    spyOn(component, 'updateBreakpoint');
     component.onResize();
     // expect(component.updateBreakpoint).toHaveBeenCalled();
   });
 
   it('should run #updateBreakpoint()', async () => {
     component.myModal = component.myModal || {};
-    component.myModal.setCurrentBreakpoint = jest.fn();
+    spyOn(component.myModal, 'setCurrentBreakpoint');
     component.updateBreakpoint();
     // expect(component.myModal.setCurrentBreakpoint).toHaveBeenCalled();
   });
 
   it('should run #closePopup()', async () => {
     component.popup = component.popup || {};
-    component.popup.remove = jest.fn();
+    spyOn(component.popup, 'remove');
     component.closePopup();
     // expect(component.popup.remove).toHaveBeenCalled();
   });
@@ -388,27 +388,27 @@ describe('MapModalComponent', () => {
     component.savedPlacesService.savePlace = {
       next: function() {}
     };
-    component.cancelNavigateModal = jest.fn();
+    spyOn(component, 'cancelNavigateModal');
     component.savePlace();
     // expect(component.cancelNavigateModal).toHaveBeenCalled();
   });
 
   it('should run #cancelNavigateModal()', async () => {
     component.navigateModal = component.navigateModal || {};
-    component.navigateModal.dismiss = jest.fn();
+    spyOn(component.navigateModal, 'dismiss');
     component.cancelNavigateModal();
     // expect(component.navigateModal.dismiss).toHaveBeenCalled();
   });
 
   it('should run #ngOnDestroy()', async () => {
     component.navigateToPlaceSubscription = component.navigateToPlaceSubscription || {};
-    component.navigateToPlaceSubscription.unsubscribe = jest.fn();
+    spyOn(component.navigateToPlaceSubscription, 'unsubscribe');
     component.savedPlacesService = component.savedPlacesService || {};
     component.savedPlacesService.navigateToPlace = {
       next: function() {}
     };
     component.MapSubscription = component.MapSubscription || {};
-    component.MapSubscription.unsubscribe = jest.fn();
+    spyOn(component.MapSubscription, 'unsubscribe');
     component.ngOnDestroy();
     // expect(component.navigateToPlaceSubscription.unsubscribe).toHaveBeenCalled();
     // expect(component.MapSubscription.unsubscribe).toHaveBeenCalled();
@@ -416,7 +416,7 @@ describe('MapModalComponent', () => {
 
   it('should run #goToReport()', async () => {
     component.router = component.router || {};
-    component.router.navigate = jest.fn();
+    spyOn(component.router, 'navigate');
     component.goToReport();
     // expect(component.router.navigate).toHaveBeenCalled();
   });
