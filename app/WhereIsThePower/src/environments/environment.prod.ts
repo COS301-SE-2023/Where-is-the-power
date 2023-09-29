f8fdf97645c3cbfc3921cca60f70eaf7c430dba7
Andreas Visagie u21430901 <angryaxe.visagie@gmail.com>
2023-09-29 07:26:06 +0200

    Remove old key and set the prod secret

--- app/WhereIsThePower/src/environments/environment.prod.ts
+++ app/WhereIsThePower/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  MapboxApiKey: 'pk.eyJ1IjoiZG9jdG9yYnVpbGRlciIsImEiOiJjbGtjZGtzYWQwMDdtM3Bsdm90bDJ6dmFpIn0.Z3CBinZUO0h3Pi96_8wBPw'
+  MapboxApiKey: 'HelloAPIKey'
 };
--- app/WhereIsThePower/src/environments/environment.ts
+++ app/WhereIsThePower/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  MapboxApiKey: 'pk.eyJ1IjoidTE4MDA0ODc0IiwiYSI6ImNsajMzdWh5ZzAwcHAzZXMxc3lveXJmNDgifQ.7P_tuuiC4M_Q1_H5ZF1rTA'
+  MapboxApiKey: 'HelloAPIKey'
 };
